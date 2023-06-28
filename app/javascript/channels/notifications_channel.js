0295530525b4d576708495260334c35d61813c5d
nicolas-m-rey <nic.m.rey@gmail.com>
2023-06-28 14:49:18 -0700

    ActionCable configuration

--- app/javascript/channels/notifications_channel.js
+++ app/javascript/channels/notifications_channel.js
@@ -3,6 +3,7 @@ import consumer from "channels/consumer"
 consumer.subscriptions.create("NotificationsChannel", {
   connected() {
     // Called when the subscription is ready for use on the server
+    console.log("connected to notifications channel")
   },
 
   disconnected() {
@@ -11,5 +12,19 @@ consumer.subscriptions.create("NotificationsChannel", {
 
   received(data) {
     // Called when there's incoming data on the websocket for this channel
-  }
+    this.displayNotification(data);
+  },
+
+  displayNotification(data) {
+    if (!("Notification" in window)) {
+      alert("This browser does not support desktop notification");
+    } else if (Notification.permission === "granted") {
+      console.log("Notification granted");
+      var notification = new Notification(data.title, { body: data.body });
+    } else if (Notification.permission !== "denied") {
+      console.log("Notification permissions need to be requested");
+    } else {
+      console.warn("Notification denied");
+    }
+  },
 });
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,10 @@
 class Post < ApplicationRecord
+    after_create :notify_users
+
+    def notify_users
+        ActionCable.server.broadcast("notifications_channel", {
+            title: self.title,
+            body: self.body
+        })
+      end
 end
